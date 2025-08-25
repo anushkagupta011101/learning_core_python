28531900cc3b15fe5db51e5f4065b2d86e95af56
anushkagupta011101 <anushka0tanu@gmail.com>
2025-08-25 21:36:02 +0530

    An overlook to the python

---
+++ Introduction to python.py
@@ -0,0 +1,57 @@
+# ______________________________________________________________________________________
+# Author: Anushka Gupta
+
+# -------------------------------------------- INTRODUCTION TO PYTHON ---------------------------------------------------
+
+# ******** WHAT IS PYTHON? **********
+
+# 1. Python is general purpose high level programming language.
+# 2. It is an interpreted interactive, object-oriented programming language.
+# 3. It is designed to be highly readable, with a syntax that emphasizes simplicity and clarity.
+# 4. It was created by GUIDO VAN ROSSUM AND WAS RELEASED IN 1991.He was born in Haarlem, Netherlands on 31st January,1956.
+
+# ********** USES OF PYTHON ***************
+
+# 1. Data Science and Data Analytics
+# 2. web development and software development
+# 3. Machine learning and Artificial intelligence
+# 4. Automation and Scripting
+
+
+# *********** What can Python do? **************
+
+# 1. Python can be used on a server to create web applications.
+# 2. Python can be used alongside software to create workflows.
+# 3. Python can connect to database systems. It can also read and modify files.
+# 4. Python can be used to handle big data and perform complex mathematics.
+# 5. Python can be used for rapid prototyping, or for production-ready software development.
+
+
+
+# *************** Why Python? *********************
+
+# 1. Python works on different platforms (Windows, Mac, Linux, Raspberry Pi, etc).
+# 2. Python has a simple syntax similar to the English language.
+# 3. Python has syntax that allows developers to write programs with fewer lines than 
+# 	some other programming languages.
+# 4. Python runs on an interpreter system, meaning that code can be executed as soon as 
+# 	it is written. This means that prototyping can be very quick.
+# 5. Python can be treated in a procedural way, an object-oriented way or a functional way.
+
+
+
+# **************** How to Install Python? *********************
+
+#  1. Go to: https://www.python.org/downloads
+#  2. Download the latest version for your operating system
+#  3. Run the installer and make sure to check the box: "Add Python to PATH"
+#  4. After installation, open your terminal and type `python --version` to verify
+
+
+# ****************** Python IDEs and Editors ************************
+
+# You can write Python code using various tools:
+# - VS Code (most popular, lightweight)
+#  - PyCharm (feature-rich IDE)
+# - Jupyter Notebook (best for data science)
+# - IDLE (comes built-in with Python)
