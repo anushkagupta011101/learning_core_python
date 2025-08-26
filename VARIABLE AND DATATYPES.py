d5ab5734d5de73d8bfadea945ddf015795e118e0
anushkagupta011101 <anushka0tanu@gmail.com>
2025-08-26 21:54:15 +0530

    Add files via upload
    understanding variables and data types

---
+++ VARIABLE AND DATATYPES.py
@@ -0,0 +1,80 @@
+#--------------------------------------------------------------------------------------------------------------------------------
+
+#Author:ANUSHKA GUPTA
+
+#**********************************************VARIABLES AND DATATYPE***************************************************************
+
+
+# VARIABLE: It is a short name of any value.
+#                 or
+#     It is like a container where we can store values.
+
+#------------------------------------------------------------------------------
+
+# TYPES OF VARIABLE :
+# According to function, variable are of two types:
+
+# 1. GLOBAL VARIABLE: a variable declared at the top level of a script or module, making it accessible throughout that module.
+
+# 2. LOCAL VARIABLE:  Local variables are defined inside of a function, and they only exist inside that function.
+
+
+#--------------------------------------------------------------------------------
+# LIMITATIONS OF VARIABLES:
+
+# 1. VARIABLE SHOULD START WITH ALPHABET OTHERWISE PYTHON WILL NOT ACCEPT AS VARIABLE.
+# EX. X=2
+
+# 2. VARIABLE IS CASE SENSTIVE.
+# EX. X=2
+#     x=3
+
+# 3. VARIABLES CAN'T CONTAIN ANY SPECIAL CHARACTER.
+# EX. R$M=45 ❌
+#     RAM=45 ✅
+
+# 4. VARIABLE CAN BE ALPHA-NUMBERIC i.e FIRST ALPHABET THEN NUMBER
+# EX. A2=4
+
+# 5. VARIABLE CAN CONTAIN (_) UNDERSCORE .
+# EX. A_2="TANU"
+
+#**************************************** DATATYPES ***********************************
+
+# DATATYPES: It shows the behavior of data.
+
+# TYPES:
+
+# 1. NUMERICAL DATA:
+#                 i)INTEGER: All the whole number lies in it.
+#                 ex. 1,2,3,4,5
+
+#                 ii)FLOAT: All the decimal values lies in it.
+#                 ex. 4.5,3.4,2.3,5.6
+
+#                 iii) COMPLEX: Any number which contain (j) at the end are complex
+#                 ex. 45j.67j,89j,90j
+
+# 2. TEXT DATA: 
+# STRING(STR): It is always written in " " or' '
+
+# 3.SEQUENCE DATA:
+#             i) LIST:
+#             ii) TUPLES:
+#             iii)RANGE:
+
+# 4. SET data: set
+
+# 5. MAPPING data: dict
+
+# 6. BOOLEAN DATA:
+#             i) True: when statement is true and its mathematical representation is 1.
+#             ii) False:when statement is false and its mathematical representation is 0.
+
+# 7. NONE DATA: 
+
+# 8. BINARY DATA:
+
+
+
+
