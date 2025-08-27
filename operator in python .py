257b344f431f761b80d315f7496f963b9f9b3949
anushkagupta011101 <anushka0tanu@gmail.com>
2025-08-27 23:27:13 +0530

    operator
    operator

---
+++ operator in python .py
@@ -0,0 +1,67 @@
+#----------------------------------------------------------------------------------------------------------------------------------
+#author: ANUSHKA GUPTA
+
+#---------------------------------------------------------------------------------------------------------------------------------
+
+#*********************************************** OPERATORS ******************************************************************
+
+
+
+
+# Arithematic operation {+,_,*,/,%,//,**}
+
+# (+): it shows the addition of two or more than two numbers
+# x=56
+# y=78
+# z=x+y
+# print(z)
+
+
+# (-): it shows the subtraction of two numbers
+# x=100
+# y=67
+# z=x-y
+# print(z)
+
+# (*): it shows the multiplication of two numbers
+# x=34
+# y=6
+# z=x*y
+# print(z)
+
+#(/): it shows the division 
+# x=56
+# y=7
+# z=x/y
+# print(z)    #gives float 
+# print(int(z))     # gives integer
+
+
+
+# # % modules: it shows reminder
+
+# # x=25
+# # y=25
+# # z=x%y
+# # print(z)
+
+# # // float division: it shows value before the decimal after division
+# print(10//4)
+
+# # **: it adss power or any number like square,cube and custom
+# x=9
+# print(x**2)
+
+# # write a python code program to show the area of circle with radius 14c,
+
+# # x=14
+# # a=22/7*(x*x)
+# # print(a)
+
+
+# # python follows BODMAS rule
+# # x=10
+# # y=2
+# # z=3
+# # s=x+y*z/2
+# # print(s)
