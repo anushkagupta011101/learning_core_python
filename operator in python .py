c3d8a02e18f454ed1f572d51d571ab190e9caf26
anushkagupta011101 <anushka0tanu@gmail.com>
2025-08-28 22:28:37 +0530

    Update operator in python .py

--- operator in python .py
+++ operator in python .py
@@ -64,4 +64,97 @@
 # # y=2
 # # z=3
 # # s=x+y*z/2
+
 # # print(s)
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+comparison operator
+ 		operator 				name
+ 		>						greater than
+ 		<						less than
+ 		==						equal to
+ 		!=						not equal to
+ 		>=						greater than or equal to
+ 		<=						less than or equal to'''
+
+#greator than  (>)
+'''x=12
+y=10
+if x<y:
+	print("true")
+elif x<y:
+	print("this is right")
+else:
+	print("nothing is true")'''
+
+#less than  (<)
+'''x=45
+y=15
+if x<y:
+	print("this is true")
+elif x>y:
+	print("prince is right")'''
+
+
+# equal to (==)
+'''x=67
+z=67
+y=67
+if x==y==z:
+	print("chhotu")'''
+# not equal to  (!=)
+'''x=23
+y=23
+z=34
+if x==y==z:
+	print("yes")
+elif x==y!=z:
+	print("true")
+#greator than or equal to /less than or equal to
+x=10
+y=12
+z=12
+if y>x and y==z:
+	print("this is true")'''
+#[note:- we can use elif or else on condition]
+
+
+'''Python assigment operators
+ =	assign
++=	add and assign
+-=	substract and assign
+*=	multiplication and assign
+/= 	divide and assign
+%=	modules and assign
+**= exponentation and assign
+//= floor divison and assign'''
+
+ # assign   (=)
+'''x=13
+x+=12
+print(x)
+#sbstraction and assign
+x=25
+x-=30
+print(x)
+#multiplication and assign
+x=10
+x*=15
+print(x)
+#divison and assign
+x=15
+x/=5
+print(x)
+#modulus and assign
+x=30
+x%=8
+print(x)
+#Floor divisiable and assign
+x=45
+x//=8
+print(x)
+#Exponentation and assign
+x=6
+x**=3
+print(x)'''
